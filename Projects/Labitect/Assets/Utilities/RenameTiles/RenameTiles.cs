8fae58b7cc04095c80653aea66e47b27c053f5f0
Nicate <kirk@kirkwarez.com>
2016-12-10 19:59:29 +0100

    Added some ridiculous tools because I'm a huge nerd.

---
+++ Projects/Labitect/Assets/Utilities/RenameTiles/RenameTiles.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class RenameTiles : Editor {
+	[MenuItem("/Labitect/Rename tiles")]
+	public static void renameTiles() {
+		GameObject floor = GameObject.Find("Floor");
+
+		if(floor != null) {
+			for(int index = 0; index < floor.transform.childCount; index++) {
+				floor.transform.GetChild(index).name = "Tile " + (index + 1);
+			}
+		}
+	}
+}
---
+++ Projects/Labitect/Assets/Utilities/RenameWalls/RenameWalls.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class RenameWalls : Editor {
+	[MenuItem("/Labitect/Rename walls")]
+	public static void renameTiles() {
+		GameObject walls = GameObject.Find("Walls");
+
+		if(walls != null) {
+			for(int index = 0; index < walls.transform.childCount; index++) {
+				walls.transform.GetChild(index).name = "Wall " + (index + 1);
+			}
+		}
+	}
+}

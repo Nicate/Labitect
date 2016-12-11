25f4379c27a32445c01cabec26b8100b5a3d8455
Nicate <kirk@kirkwarez.com>
2016-12-11 17:24:23 +0100

    Added a rudimentary inventory system.

---
+++ Projects/Labitect/Assets/Scripts/Inventory/Inventory.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour {
+	public List<GameObject> inventory;
+
+	void Start() {
+		inventory = new List<GameObject>();
+	}
+}

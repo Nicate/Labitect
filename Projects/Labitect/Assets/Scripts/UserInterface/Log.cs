b6c69ce5b270c5d21cc00018f9bdc9fbda94a1db
Nicate <kirk@kirkwarez.com>
2016-12-11 23:40:51 +0100

    Added user interface stuff for the game flow.

---
+++ Projects/Labitect/Assets/Scripts/UserInterface/Log.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Log : MonoBehaviour {
+	public Text logText;
+
+
+	public void log(string message) {
+		if(logText.text == "") {
+			logText.text = message;
+		}
+		else {
+			logText.text = logText.text + Environment.NewLine + message;
+		}
+	}
+}
---
+++ Projects/Labitect/Assets/Scripts/UserInterface/UserInterface.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class UserInterface : MonoBehaviour {
+	public GameObject[] gameObjectsToDisable;
+	public GameObject[] gameObjectsToEnable;
+
+
+	public void Start() {
+		foreach(GameObject gameObject in gameObjectsToDisable) {
+			gameObject.SetActive(true);
+		}
+		
+		foreach(GameObject gameObject in gameObjectsToEnable) {
+			gameObject.SetActive(false);
+		}
+	}
+
+
+	public void toggleUserInterface() {
+		foreach(GameObject gameObject in gameObjectsToDisable) {
+			gameObject.SetActive(false);
+		}
+		
+		foreach(GameObject gameObject in gameObjectsToEnable) {
+			gameObject.SetActive(true);
+		}
+	}
+}

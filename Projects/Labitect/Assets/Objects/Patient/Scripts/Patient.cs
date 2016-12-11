46ce5ec43d39b187a8e7cc84f1e2b15903ef4e3e
Nicate <kirk@kirkwarez.com>
2016-12-11 16:22:48 +0100

    Set up bodies with pocket status, added navigation mesh agent to patients, set up basic test.

--- Projects/Labitect/Assets/Objects/Patient/Scripts/Patient.cs
+++ Projects/Labitect/Assets/Objects/Patient/Scripts/Patient.cs
@@ -1,12 +1,23 @@
 ﻿using UnityEngine;
 
 public class Patient : MonoBehaviour {
+	public GameObject[] heads;
+
+	public GameObject[] straightjackets;
+	public GameObject[] bodiesWithoutPockets;
+	public GameObject[] bodiesWithPockets;
+
+
 	public void Awake() {
 		
 	}
 
 	public void Start() {
-		
+		var bla = GetComponent<NavMeshAgent>();
+		bla.destination = Vector3.zero;
+
+		GameObject head = Instantiate(heads[0], transform, false) as GameObject;
+		GameObject body = Instantiate(straightjackets[0], transform, false) as GameObject;
 	}
 
 	public void Update() {

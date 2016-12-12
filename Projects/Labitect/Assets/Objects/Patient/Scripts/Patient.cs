bcb0202bdf8e44d7491d73e8bd1eabdf99eed2a0
Nicate <kirk@kirkwarez.com>
2016-12-12 23:10:31 +0100

    Build some kind of gradientbased light finder... it's a bit untuned >_>

--- Projects/Labitect/Assets/Objects/Patient/Scripts/Patient.cs
+++ Projects/Labitect/Assets/Objects/Patient/Scripts/Patient.cs
@@ -17,11 +17,15 @@ public class Patient : MonoBehaviour {
 	/// </summary>
 	public int numberOfPockets;
 
-	public List<GameObject> inventory;
+	public List<Item> inventory;
 
 	private Inventory[] inventories;
 
 	private WaitForSeconds wait = new WaitForSeconds(0.1f);
+
+	private Item desire;
+	private Inventory destination;
+	private Patient target;
 
 
 	public void Awake() {
@@ -56,12 +60,94 @@ public class Patient : MonoBehaviour {
 		while(true) {
 			yield return wait;
 
-			hunger -= 0.1f;
+			hunger += 0.001f;
 
-			if(hunger < 0.5) {
-				Inventory inventory = getBestInventoryForFood();
-				move(inventory.transform.position);
+			calm = Mathf.Clamp01(calm);
+			fear = Mathf.Clamp01(fear);
+			hunger = Mathf.Clamp01(hunger);
+			health = Mathf.Clamp01(health);
+
+			if(hunger > 0.5 && hasSpace()) {
+				Item food;
+				Inventory inventory;
+				if(getBestFood(out inventory, out food)) {
+					moveToGet(inventory, food);
+				}
 			}
+
+			moveToLight();
+		}
+	}
+
+
+	private void moveToGet(Inventory inventory, Item item) {
+		desire = item;
+		destination = inventory;
+		target = null;
+
+		move(inventory.transform.position);
+	}
+	
+	private void moveToGet(Patient patient, Item item) {
+		desire = item;
+		destination = null;
+		target = patient;
+
+		move(patient.transform.position);
+	}
+
+
+	private void moveToLight() {
+		float desiredLight = 2.0f - Mathf.Pow(2.0f, fear);
+
+		float bestSample = float.MaxValue;
+		Vector3 bestSamplePosition = transform.position;
+
+		for(int count = 0; count < 9; count++) {
+			float angle = count * 360.0f / 8.0f;//Random.Range(-180.0f, 180.0f);
+
+			// Adjust the speed for the rate the coroutine runs at.
+			Vector3 samplePosition = transform.position + Quaternion.AngleAxis(angle, Vector3.up) * Vector3.forward;
+
+			// Hackity.
+			if(count == 9) {
+				samplePosition = transform.position;
+			}
+
+			float sample = getLight(samplePosition);
+
+			float gradient = Mathf.Abs(sample - desiredLight);
+
+			if(gradient < bestSample) {
+				bestSample = gradient;
+				bestSamplePosition = samplePosition;
+			}
+		}
+
+		move(bestSamplePosition);
+	}
+	
+	private float getLight(Vector3 position) {
+		float light = 0.0f;
+		int lights = 0;
+
+		foreach(Inventory inventory in inventories) {
+			float distance = Vector3.Distance(inventory.transform.position, position);
+
+			if(distance > 0.0) {
+				light += inventory.lamp / distance;
+
+				if(inventory.lamp > 0.0f) {
+					lights++;
+				}
+			}
+		}
+
+		if(lights == 0) {
+			return 0.0f;
+		}
+		else {
+			return light / lights;
 		}
 	}
 
@@ -71,22 +157,40 @@ public class Patient : MonoBehaviour {
 	}
 
 
-	private Inventory getBestInventoryForFood() {
+	private bool getBestFood(out Inventory inventory, out Item food) {
 		float bestFoodValue = float.MinValue;
 		Item bestFood = null;
 		Inventory bestInventory = null;
 
-		foreach(Inventory inventory in inventories) {
-			foreach(Item item in inventory.inventory) {
+		foreach(Inventory currentInventory in inventories) {
+			foreach(Item item in currentInventory.inventory) {
 				if(item.food > bestFoodValue) {
 					bestFoodValue = item.food;
 					bestFood = item;
-					bestInventory = inventory;
+					bestInventory = currentInventory;
 				}
 			}
 		}
 		
-		return bestInventory;
+		food = bestFood;
+		inventory = bestInventory;
+
+		return bestFood != null;
+	}
+
+
+	private Item selectBestWeapon() {
+		float bestWeaponValue = float.MinValue;
+		Item bestWeapon = null;
+
+		foreach(Item item in inventory) {
+			if(bestWeaponValue < item.weapon) {
+				bestWeaponValue = item.weapon;
+				bestWeapon = item;
+			}
+		}
+
+		return bestWeapon;
 	}
 
 

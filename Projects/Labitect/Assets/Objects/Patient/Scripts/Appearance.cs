6f210870cbe43f4433d618309d8c513b9f9e1e14
Nicate <kirk@kirkwarez.com>
2016-12-12 01:32:50 +0100

    Patients now have speed and strength stats, and they beef and spry up visually, also affecting their navigation.
    I had to reexport the head and move it up in the prefab so I could scale it.

--- Projects/Labitect/Assets/Objects/Patient/Scripts/Appearance.cs
+++ Projects/Labitect/Assets/Objects/Patient/Scripts/Appearance.cs
@@ -9,35 +9,45 @@ public class Appearance : MonoBehaviour {
 
 
 	public void Start() {
-		if(heads.Length > 0) {
-			Instantiate(heads[Random.Range(0, heads.Length)], transform, false);
-		}
-
-		int numberOfPockets = Random.Range(0, 3);
-
-		if(numberOfPockets == 0) {
-			if(straightjackets.Length > 0) {
-				Instantiate(straightjackets[Random.Range(0, straightjackets.Length)], transform, false);
-			}
-		}
-		else if(numberOfPockets == 1) {
-			if(bodiesWithoutPockets.Length > 0) {
-				Instantiate(bodiesWithoutPockets[Random.Range(0, bodiesWithoutPockets.Length)], transform, false);
-			}
-		}
-		else if(numberOfPockets == 2) {
-			if(bodiesWithPockets.Length > 0) {
-				Instantiate(bodiesWithPockets[Random.Range(0, bodiesWithPockets.Length)], transform, false);
-			}
-
-			// Add extra pocket.
-			numberOfPockets = 3;
-		}
-
 		Patient patient = GetComponent<Patient>();
-		
-		if(patient != null) {
-			patient.numberOfPockets = numberOfPockets;
+		NavMeshAgent navMeshAgent = GetComponent<NavMeshAgent>();
+
+		// Set up the patient's appearance.
+		GameObject head = null;
+		GameObject body = null;
+
+		if(heads.Length > 0) {
+			head = heads[Random.Range(0, heads.Length)];
 		}
+
+		if(patient.numberOfPockets == 0) {
+			if(straightjackets.Length > 0) {
+				body = straightjackets[Random.Range(0, straightjackets.Length)];
+			}
+		}
+		else if(patient.numberOfPockets == 1) {
+			if(bodiesWithoutPockets.Length > 0) {
+				body = bodiesWithoutPockets[Random.Range(0, bodiesWithoutPockets.Length)];
+			}
+		}
+		else if(patient.numberOfPockets == 2) {
+			if(bodiesWithPockets.Length > 0) {
+				body = bodiesWithPockets[Random.Range(0, bodiesWithPockets.Length)];
+			}
+
+			// Add an extra pocket.
+			patient.numberOfPockets = 3;
+		}
+
+		GameObject headInstance = Instantiate(head, transform, false) as GameObject;
+		GameObject bodyInstance = Instantiate(body, transform, false) as GameObject;
+
+		// Beef up the patient.
+		float beef = Mathf.Pow(1.5f, patient.strength);
+		
+		headInstance.transform.localScale = Vector3.Scale(headInstance.transform.localScale, new Vector3(beef, beef, beef));
+		bodyInstance.transform.localScale = Vector3.Scale(bodyInstance.transform.localScale, new Vector3(beef, 1.0f, beef));
+		
+		navMeshAgent.radius = navMeshAgent.radius * beef;
 	}
 }
--- Projects/Labitect/Assets/Objects/Patient/Scripts/Patient.cs
+++ Projects/Labitect/Assets/Objects/Patient/Scripts/Patient.cs
@@ -1,6 +1,12 @@
 ﻿using UnityEngine;
 
 public class Patient : MonoBehaviour {
+	[HideInInspector]
+	public float speed;
+
+	[HideInInspector]
+	public float strength;
+
 	/// <summary>
 	/// Pocket 0 is what they have in their hands.
 	/// </summary>
@@ -9,11 +15,19 @@ public class Patient : MonoBehaviour {
 
 
 	public void Awake() {
-		
+		// Generate these here so scripts can use their values in Start().
+		speed = Random.Range(0.0f, 1.0f);
+		strength = Random.Range(0.0f, 1.0f);
+		numberOfPockets = Random.Range(0, 3);
 	}
 
 	public void Start() {
+		NavMeshAgent navMeshAgent = GetComponent<NavMeshAgent>();
 
+		float spry = Mathf.Pow(2.0f, speed);
+
+		navMeshAgent.speed = navMeshAgent.speed * spry;
+		navMeshAgent.angularSpeed = navMeshAgent.angularSpeed * spry;
 	}
 
 	public void Update() {

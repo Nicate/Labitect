87ed178b19faa38591c1d628179ecb2ce0d544be
Nicate <kirk@kirkwarez.com>
2016-12-22 07:32:56 +0100

    Added a way for me to inspect and edit (while playing, for debug purposes) properties that are not meant as configurable properties. They are simply properties but they are rendered with a red background so I know which ones are which. You can also use this on a private property together with [SerializeField].

---
+++ Projects/Labitect/Assets/Editor/DebugProperty/DebugPropertyDrawer.cs
@@ -0,0 +1,23 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomPropertyDrawer(typeof(DebugPropertyAttribute))]
+public class DebugPropertyDrawer : PropertyDrawer {
+	/// <summary>
+	/// The color used for debug properties.
+	/// </summary>
+	private static readonly Color color = new Color(1.0f, 0.5f, 0.5f);
+
+
+	/// <summary>
+	/// Displays the background in the debug color to indicate this is a debug property.
+	/// </summary>
+	/// <param name="position">The position of the property.</param>
+	/// <param name="property">The property to display with the debug color as background.</param>
+	/// <param name="label">The label of the property.</param>
+	public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
+		GUI.backgroundColor = color;
+
+		EditorGUI.PropertyField(position, property, label, true);
+	}
+}
---
+++ Projects/Labitect/Assets/Scripts/DebugProperty/DebugPropertyAttribute.cs
@@ -0,0 +1,5 @@
+﻿using UnityEngine;
+
+public class DebugPropertyAttribute : PropertyAttribute {
+	// This class only has to exist. See Assets/Editor/DebugPropertyDrawer for more information.
+}
